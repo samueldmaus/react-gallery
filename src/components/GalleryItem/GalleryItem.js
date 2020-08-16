1cec014e922ee71c2c66932be3bc28db08b0ee05
Sam Maus <samueldmaus@gmail.com>
2020-08-15 20:40:16 -0500

    added conditional rendering to flip between picture and description

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 
 class GalleryItem extends Component{
+    state = {
+        showDescription: true
+    };
+
+    changeDescripton=()=>{
+        this.setState({
+            showDescription: !this.state.showDescription
+        })
+    }
+
     render(){
         return(
             <div>
-            <img src={this.props.image.path} alt={this.props.image.description} />
+            {this.state.showDescription ? <img onClick={this.changeDescripton} src={this.props.image.path} 
+            alt={this.props.image.description}/> : <p onClick={this.changeDescripton}>{this.props.image.description}</p>}
             <br/>
             <button onClick={()=>this.props.updateLikes(this.props.image)}>Love It!</button>
             <p>{this.props.image.likes} people love this!</p>
