73365c541ec3ef9d7b19e7356f44d6bc55e80dd5
Sam Maus <samueldmaus@gmail.com>
2020-08-16 00:29:18 -0500

    added material ui to delete and like buttons

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import './GalleryItem.css';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { IconButton } from '@material-ui/core';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 class GalleryItem extends Component{
     state = {
@@ -18,8 +21,15 @@ class GalleryItem extends Component{
             {this.state.showDescription ? <img onClick={this.changeDescripton} src={this.props.image.path} 
             alt={this.props.image.description}/> : <p onClick={this.changeDescripton}>{this.props.image.description}</p>}
             <br/>
-            <button className="updateBtn" onClick={()=>this.props.updateLikes(this.props.image)}>Love It!</button>
-            <button className="deleteBtn" onClick={()=>this.props.deleteMovie(this.props.image)}>Delete</button>
+           
+            <IconButton onClick={()=>this.props.updateLikes(this.props.image)} aria-label="favorite">
+                <FavoriteIcon/></IconButton>
+            
+
+            <IconButton onClick={()=>this.props.deleteMovie(this.props.image)} aria-label="delete">
+                <DeleteIcon/></IconButton>
+
+            
             <p>{this.props.image.likes} people love this!</p>
             </div>
         )
